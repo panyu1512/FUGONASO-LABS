2ac66f42e2b61ddf7cb0d98284df847456eac920
Enrique <enrique.ferrer@espossible.com>
2022-04-26 20:28:23 +0200

    autoplay video

--- src/frontend/components/Pages/Home/Home.js
+++ src/frontend/components/Pages/Home/Home.js
@@ -5,7 +5,7 @@ import { Button } from 'react-bootstrap';
 function Home() {
   return (
     <div className='hero-container'>
-        <video src={videoHome}/>
+        <video src={videoHome} autoplay loop muted/>
         <h1>FUGONASO LABS</h1>
         <p>NFT COLECTION</p>
         <div className='button'>
